edf0625a6fa01c70ccd3f6279770b72ac95d0d5b
pookdeveloper <pookdeveloper@gmail.com>
2017-03-30 16:05:48 +0200

    asd

--- JS EVENTS TODO 1 Assignment/js/index.js
+++ JS EVENTS TODO 1 Assignment/js/index.js
@@ -1,4 +1,9 @@
 
+
+document.getElementById("1").addEventListener("click", verBoton, false);
+
+document.getElementById("2").addEventListener("click", verBoton, false)
+
 function verBoton(elemento) {
-    alert('BOTON ' + elemento.id);
+    alert('BOTON ' + elemento.target.id);
 }
